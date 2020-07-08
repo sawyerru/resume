e814659dcaa210d56c1b623dda88d8975f30f265
Sawyer Ruben <sawyer.ruben@gmail.com>
2020-07-08 12:01:50 -0400

    generated deployables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { AboutMeComponent } from './components/body/about-me/about-me.component';
+import { EducationComponent } from './components/body/education/education.component';
+import { ExperiencesComponent } from './components/body/experiences/experiences.component';
+import { ProjectsComponent } from './components/body/projects/projects.component';
+import { InterestsComponent } from './components/body/interests/interests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    EducationComponent,
+    ExperiencesComponent,
+    ProjectsComponent,
+    InterestsComponent,
+    AboutMeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'about-me', component: AboutMeComponent},
+      {path: 'education', component: EducationComponent},
+      {path: 'experiences', component: ExperiencesComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: 'hobbies', component: InterestsComponent},
+      {path: '', redirectTo: '/about-me', pathMatch: 'full'},
+      {path: '**', component: AboutMeComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
